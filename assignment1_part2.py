37dff6349afcbbded21d1b8fe3f9beadeb4eb53f
Yurtledaturtle <mkoski6@gmail.com>
2022-02-13 20:12:23 -0600

    committed changes assignment1 complete

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,18 @@
+#created a class called 'book' and then a function that takes in 2 attributes (title and author).
 
+#created a function that takes in an author and a title, and sets them to the object variables.
 class Book:
+    author = ""
+    title = ""
     def __init__(self, author, title):
-        pass
-    
+        self.author = author
+        self.title = title
     def display(self):
-        pass
+        print(self.title+" written by "+self.author)
 
-
+#instantiated two objects from this class and then print them by using if __name__ == "__main__"
 if __name__ == "__main__":
-    pass
+    book1 = Book("John Steinbeck","Of Mice and Men")
+    book2 = Book("Harper Lee","To Kill a Mockingbird")
+    book1.display()
+    book2.display()
